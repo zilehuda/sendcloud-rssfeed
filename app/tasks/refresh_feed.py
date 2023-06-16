0f46ccedf362bbb98cee3266753b91ecd9d0325b
Zilehuda Tariq <zilehuda.tariq@gmail.com>
2023-06-16 17:11:45 +0500

    retry and complete status

--- app/tasks/refresh_feed.py
+++ app/tasks/refresh_feed.py
@@ -1,4 +1,5 @@
 from app.celery_app import app
+from app.constants import FetchStatus
 from app.database import get_db
 from app.models import Feed
 from app.services.rss_feed_services import RSSFeedUpdater
@@ -6,9 +7,11 @@ from app.services.rss_feed_services import RSSFeedUpdater
 
 @app.task(bind=True, name="refresh_feed")
 def refresh_feed(self, feed_id: int):
+    # TODO: split stuff into services and repositories.
     try:
         db = next(get_db())
         feed = db.query(Feed).filter_by(id=feed_id).first()
+        raise Exception("intentionally")
         if feed:
             rss_feed_service = RSSFeedUpdater(db, feed.id)
             rss_feed_service.fetch_and_update_feed()
@@ -16,6 +19,12 @@ def refresh_feed(self, feed_id: int):
         # Retry the task with a back-off mechanism
         delays = [120, 300, 480]  # Delays in seconds (2, 5, and 8 minutes)
         retry_count = self.request.retries
+        if retry_count == 0:
+            feed.fetch_status = FetchStatus.RETRYING.value
+            db.commit()
         if retry_count < len(delays):
             delay = delays[self.request.retries]
             self.retry(exc=exc, countdown=delay)
+        else:
+            feed.fetch_status = FetchStatus.FAILED.value
+            db.commit()
--- app/tasks/refresh_feeds.py
+++ app/tasks/refresh_feeds.py
@@ -2,6 +2,7 @@ from app.celery_app import app
 from app.constants import FetchStatus
 from app.database import get_db
 from app.models import Feed
+from .refresh_feed import refresh_feed
 
 
 @app.task(name="refresh_feeds")
@@ -9,5 +10,4 @@ def refresh_feeds():
     db = next(get_db())
     feeds = db.query(Feed).filter_by(fetch_status=FetchStatus.COMPLETED.value).all()
     for feed in feeds:
-        print("haha")
-        # refresh_feed.delay(feed.id)
+        refresh_feed.delay(feed.id)
