a68e2ef8259a82032d53c451b9dc56bb6271fb7a
Zilehuda Tariq <zilehuda.tariq@gmail.com>
2023-06-16 18:22:12 +0500

    update

--- app/services/rss_feed_services/feed_fetcher.py
+++ app/services/rss_feed_services/feed_fetcher.py
@@ -1,18 +1,23 @@
 from typing import Optional
-
+from fastapi import HTTPException
 import feedparser
 from feedparser import FeedParserDict
 
 
 class RSSFeedFetcher:
     def __init__(self, feed_url: str) -> None:
-        self.feed_url = feed_url
+        self._feed_url = feed_url
+
+    """
+    Not implemented any retry mechanism, even on wrong url, it return data
+    with empty entries.
+    raise exception on wrong url on bozo
+    """
 
     def fetch_feed(self) -> Optional[FeedParserDict]:
-        try:
-            feed = feedparser.parse(self.feed_url)
-            return feed
-        except Exception as e:
-            # Handle the exception according to your requirements
-            print(f"Error fetching feed: {str(e)}")
-            return None
+        feed = feedparser.parse(self._feed_url)
+        if "bozo" in feed and feed.bozo:
+            raise HTTPException(
+                status_code=400, detail="The RSS feed provided is invalid."
+            )
+        return feed
